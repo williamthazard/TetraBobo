ff5a36d0acf4ccba7489e94f7bea0a989028adc9
William Hazard <105560469+williamthazard@users.noreply.github.com>
2022-11-19 16:29:16 -0500

    Update tetrabobo.lua

--- tetrabobo.lua
+++ tetrabobo.lua
@@ -1,8 +1,9 @@
 ---TetraBobo
 
 local extensions = "/home/we/.local/share/SuperCollider/Extensions"
-engine.name = util.file_exists(extensions .. "/FormantTriPTR/FormantTriPTR.sc") and 'TetraBobo' or nil
-TetraBobo = include('lib/TetraBobo_engine')
+engine.name = util.file_exists(extensions .. "/FormantTriPTR/FormantTriPTR.sc") and 'Tetrabobo' or nil
+UI = require "ui"
+Tetrabobo = include('lib/Tetrabobo_engine')
 sh = hid.connect(1)
 if sh.device then
   shnth = include("shnth/lib/shnth")
@@ -23,28 +24,28 @@ function init()
   end
   restart_message = UI.Message.new{"please restart norns"}
   if needs_restart then redraw() return end
-  TetraBobo.add_params() -- adds params via the `.add params()` function defined in TetraBobo_engine.lua
+  Tetrabobo.add_params() -- adds params via the `.add params()` function defined in TetraBobo_engine.lua
   print("tetrabobo")
 end
 
 function shnth.bar(n, d)
-  params:set('TetraBobo_firstattack',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_secondattack',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_thirdattack',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_fourthattack',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_firstrelease',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_secondrelease',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_thirdrelease',util.linlin(-2,2,0,4,d))
-  params:set('TetraBobo_fourthrelease',util.linlin(-2,2,0,4,d))
   if d > 0.2 then
       if n==1 then
-        TetraBobo.firstbartrig(params:get(TetraBobo_firstpitch))
+        params:set('Tetrabobo_firstattack',util.linlin(-2,2,0,4,d))
+        params:set('Tetrabobo_firstrelease',util.linlin(-2,2,0,4,d))
+        Tetrabobo.firstbartrig(params:get(Tetrabobo_firstpitch))
       elseif n==2 then
-        TetraBobo.secondbartrig(params:get(TetraBobo_secondpitch))
+        params:set('Tetrabobo_secondattack',util.linlin(-2,2,0,4,d))
+        params:set('Tetrabobo_secondrelease',util.linlin(-2,2,0,4,d))
+        Tetrabobo.secondbartrig(params:get(Tetrabobo_secondpitch))
       elseif n==3 then
-        TetraBobo.thirdbartrig(params:get(TetraBobo_thirdpitch))
+        params:set('Tetrabobo_thirdattack',util.linlin(-2,2,0,4,d))
+        params:set('Tetrabobo_thirdrelease',util.linlin(-2,2,0,4,d))
+        Tetrabobo.thirdbartrig(params:get(Tetrabobo_thirdpitch))
       elseif n==4 then
-        TetraBobo.fourthbartrig(params:get(TetraBobo_fourthpitch))
+        params:set('Tetrabobo_fourthattack',util.linlin(-2,2,0,4,d))
+        params:set('Tetrabobo_fourthrelease',util.linlin(-2,2,0,4,d))
+        Tetrabobo.fourthbartrig(params:get(Tetrabobo_fourthpitch))
     end
   end
 end
