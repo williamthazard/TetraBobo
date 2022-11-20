11699b5b6b0945a0d3f83fe65ba7d56d72be3405
William Hazard <105560469+williamthazard@users.noreply.github.com>
2022-11-19 19:05:38 -0500

    Update TetraBobo_engine.lua

--- lib/TetraBobo_engine.lua
+++ lib/TetraBobo_engine.lua
@@ -8,38 +8,38 @@ local specs = {
   ["firstwidth"] = controlspec.AMP,
   ["firstphase"] = controlspec.PHASE,
   ["firstamp"] = controlspec.AMP,
-  ["firstpan"] = controlspec.PAN,
+  ["panone"] = controlspec.PAN,
   ["secondpitch"] = controlspec.FREQ,
   ["secondform"] = controlspec.FREQ,
   ["secondwidth"] = controlspec.AMP,
   ["secondphase"] = controlspec.PHASE,
   ["secondamp"] = controlspec.AMP,
-  ["secondpan"] = controlspec.PAN,
+  ["pantwo"] = controlspec.PAN,
   ["thirdpitch"] = controlspec.FREQ,
   ["thirdform"] = controlspec.FREQ,
   ["thirdwidth"] = controlspec.AMP,
   ["thirdphase"] = controlspec.PHASE,
   ["thirdamp"] = controlspec.AMP,
-  ["thirdpan"] = controlspec.PAN,
+  ["panthree"] = controlspec.PAN,
   ["fourthpitch"] = controlspec.FREQ,
   ["fourthform"] = controlspec.FREQ,
   ["fourthwidth"] = controlspec.AMP,
   ["fourthphase"] = controlspec.PHASE,
   ["fourthamp"] = controlspec.AMP,
-  ["fourthpan"] = controlspec.PAN,
+  ["panfour"] = controlspec.PAN,
   ["chaos"] = controlspec.new(-24, 24, "lin", 0, 0, ""),
-  ["firstattack"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["secondattack"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["thirdattack"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["fourthattack"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["firstrelease"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["secondrelease"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["thirdrelease"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
-  ["fourthrelease"] = controlspec.new(0.003, 3, "exp", 0, 0, "s"),
+  ["firstattack"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["secondattack"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["thirdattack"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["fourthattack"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["firstrelease"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["secondrelease"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["thirdrelease"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
+  ["fourthrelease"] = controlspec.new(0.003, 3, "exp", 0, 0.003, "s"),
 }
 
 -- this table establishes an order for parameter initialization:
-local param_names = {"firstpitch","firstform","firstwidth","firstphase","firstamp","firstpan","secondpitch","secondform","secondwidth","secondphase","secondamp","secondpan","thirdpitch","thirdform","thirdwidth","thirdphase","thirdamp","thirdpan","fourthpitch","fourthform","fourthwidth","fourthphase","fourthamp","fourthpan","chaos","firstattack","secondattack","thirdattack","fourthattack","firstrelease","secondrelease","thirdrelease","fourthrelease"}
+local param_names = {"firstpitch","firstform","firstwidth","firstphase","firstamp","panone","secondpitch","secondform","secondwidth","secondphase","secondamp","pantwo","thirdpitch","thirdform","thirdwidth","thirdphase","thirdamp","panthree","fourthpitch","fourthform","fourthwidth","fourthphase","fourthamp","panfour","chaos","firstattack","secondattack","thirdattack","fourthattack","firstrelease","secondrelease","thirdrelease","fourthrelease"}
 
 -- initialize parameters:
 function Tetrabobo.add_params()
@@ -52,7 +52,7 @@ function Tetrabobo.add_params()
       id = "Tetrabobo_"..p_name,
       name = p_name,
       controlspec = specs[p_name],
-      formatter = p_name == ("firstpan" or "secondpan" or "thirdpan" or "fourthpan") and Formatters.bipolar_as_pan_widget or nil,
+      formatter = p_name == "pan" and Formatters.bipolar_as_pan_widget or nil,
       -- every time a parameter changes, we'll send it to the SuperCollider engine:
       action = function(x) engine[p_name](x) end
     }
